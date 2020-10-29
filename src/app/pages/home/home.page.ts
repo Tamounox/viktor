0d029bf993f8e96d23f63d3ac081cf5321211d1f
Gaston Antoine <antoine.gaston@ynov.com>
2020-10-29 11:35:19 +0100

    init git

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,4 @@
-import { ReglesComponent } from './../../regles/regles.component';
 import { Component } from '@angular/core';
-import { PopoverController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,15 +7,4 @@ import { PopoverController } from '@ionic/angular';
 })
 export class HomePage {
 
-  constructor(public popoverController: PopoverController) {}
-
-  async presentPopover(ev: any) {
-    const popover = await this.popoverController.create({
-      component: ReglesComponent,
-      cssClass: '',
-      event: ev,
-      translucent: true
-    });
-    return await popover.present();
-  }
 }
--- src/app/pages/players/players.page.ts
+++ src/app/pages/players/players.page.ts
@@ -1,5 +1,8 @@
 import { UserService } from './../../user.service';
 import { Component, OnInit } from '@angular/core';
+import { ReglesComponent } from './../../regles/regles.component';
+import { PopoverController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-players',
@@ -8,13 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayersPage implements OnInit {
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, public popoverController: PopoverController) { }
   test = '';
   players = [];
 
   ngOnInit() {
   }
 
+  async presentPopover(ev: any) {
+    const popover = await this.popoverController.create({
+      component: ReglesComponent,
+      cssClass: '',
+      event: ev,
+      translucent: true
+    });
+    return await popover.present();
+  }
   addPlayer() {
     console.log('players', this.players);
     this.players.push( {
